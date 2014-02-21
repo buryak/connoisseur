ef90902588c15032edec39f08f917be102e4d8a5
whoisyuri <buryak@gmail.com>
2014-02-21 09:56:28 -0500

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Connoisseur::Application.routes.draw do
+  resources :stores
   resources :products
   root "products#index"
   # The priority is based upon order of creation: first created -> highest priority.
